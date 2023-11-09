0c8aa2a12e741d2c0a9d296493741c885b5f35e3
lectrode <electrodexsnet@gmail.com>
2023-11-09 12:42:21 -0700

    v0.8.0 dynamic storage
    [details]
    * use script to get storage devices dynamically
    * use template for top process cpu and ram utilization
    
    Signed-off-by: lectrode <electrodexsnet@gmail.com>

---
+++ module_storage.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for d in $(mount|grep -oE '/dev/[0-9a-z]+ on '|sort -u|cut -d' ' -f1); do
+  p="$(mount|grep -oE "$d on [0-9a-zA-Z\/]+"|awk '{print length, $0}'|sort|head -n1|cut -d' ' -f4-)"
+  dsp="            $p"; dsp="$(echo ${dsp: -12:12}|xargs)"
+  echo "\${color1}\${goto 30}$(echo "$dsp            "|cut -c1-12): \${color}\${fs_used $p}/\${fs_size $p} \${alignr}\${fs_used_perc $p}% \${if_match 750 < \${fs_used_perc $p}0}\${color2}\$endif\${if_match 900 < \${fs_used_perc $p}0}\${color3}\$endif\${fs_bar 7,100 $p}"
+done
+
+exit 0
